e4c7b5ee62bc88e20835c90389b84bf257777398
majumderarnabagt642@gmail.com <arnab.majumder@lpu.in>
2024-07-16 20:52:34 +0530

    Cart and Sign Up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import Placeholder from './pages/PlaceOrder/PlaceOrder'
+import LoginPopup from './components/LoginPopup/LoginPopup'
 
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false)
+
   return (
     <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
       <div className='app'>
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/Cart' element={<Cart />} />
-          <Route path='/Order' element={<Placeholder />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<Placeholder />} />
         </Routes>
       </div>
       <Footer />

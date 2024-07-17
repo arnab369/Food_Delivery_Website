e0b2434bdfc791b8289505b4d2b05d824f8023b9
majumderarnabagt642@gmail.com <arnab.majumder@lpu.in>
2024-07-17 20:56:04 +0530

    Admin

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -21,4 +21,28 @@ const addFood = async (req, res) => {
     }
 }
 
-export {addFood}
+// all food list
+const listFood = async(req, res) => {
+    try {
+        const foods = await foodModel.find({});
+        res.json({success:true, data:foods})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:"Error"})
+    }
+}
+
+//remove food item
+const removeFood = async(req, res) => {
+    try {
+        const food = await foodModel.findById(req.body.id);
+        fs.unlink('uploads/${food.image}',()=>{})
+        await foodModel.findByIdAndUpdate(req.body.id);
+        res.json({success:true,message:"Food Removed"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"Error"})
+    }
+}
+
+export {addFood, listFood, removeFood}
